587f1529b5c263371300c0c980d4eba517e083dd
FMaslina <FMaslina@yandex.ru>
2023-09-11 22:26:03 +0700

    Add endpoints: registration, login, token refresh

--- currencies/users/urls.py
+++ currencies/users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from users.views import CreateUserView, TokenAuthView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
-
+    path('user/register', CreateUserView.as_view(), name='register'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/login', TokenAuthView.as_view(), name='login_jwt'),
 ]
