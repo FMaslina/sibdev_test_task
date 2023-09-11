9d022cae261d0fb9cd4ac78a403c24de1b1d68d9
FMaslina <FMaslina@yandex.ru>
2023-09-11 22:25:32 +0700

    Add CreateUserView and TokenAuthView

--- currencies/users/views.py
+++ currencies/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, ListAPIView
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from users.serializers import UserSerializer, TokenAuthSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+User = get_user_model()
+
 
 # Create your views here.
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TokenAuthView(TokenObtainPairView):
+    serializer_class = TokenAuthSerializer
+
