4c37c4627dcafd20a22443f71aec69b5fb8e6bcc
FMaslina <FMaslina@yandex.ru>
2023-09-14 13:21:08 +0700

    Add celery config

---
+++ currencies/currencies/celery.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'currencies.settings')
+
+app = Celery('currencies')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    'get_daily_quotes': {
+        'task': 'quotes.tasks.get_quotes',
+        'schedule': crontab(hour='17', minute='27')
+    },
+}
