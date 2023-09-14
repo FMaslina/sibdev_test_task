204fdf0622a407c502bd6239ec372274d9136d83
FMaslina <FMaslina@yandex.ru>
2023-09-14 19:58:52 +0700

    Add 'currency/user_currency' endpoint

--- currencies/quotes/urls.py
+++ currencies/quotes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from quotes.views import GetArchiveQuotes
+from quotes.views import GetArchiveQuotes, AddCurrencyToTracked
 
 urlpatterns = [
     path('quotes/archive', GetArchiveQuotes.as_view(), name='get_archive_quotes'),
+    path('currency/user_currency', AddCurrencyToTracked.as_view(), name='add_currency_to_tracked')
 ]
