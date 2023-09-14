b4bc9c3b06b3aedde3eb745fba0826e5a53023f9
FMaslina <FMaslina@yandex.ru>
2023-09-14 13:21:24 +0700

    Add get_quotes celery task

---
+++ currencies/quotes/tasks.py
@@ -0,0 +1,17 @@
+import requests
+from celery import shared_task
+from quotes.models import CurrencyModel, QuoteModel
+
+
+@shared_task
+def get_quotes():
+    data = requests.request("GET", "https://www.cbr-xml-daily.ru/daily_json.js").json()
+    valutes = data['Valute']
+    for currency in valutes.values():
+        currency_obj = CurrencyModel.objects.filter(currency_name=currency['CharCode'])
+        if not currency_obj.exists():
+            currency_obj = CurrencyModel.objects.create(currency_name=currency['CharCode'])
+        else:
+            currency_obj = currency_obj.first()
+        QuoteModel.objects.create(currency=currency_obj, quote=currency['Value'])
+    return data
