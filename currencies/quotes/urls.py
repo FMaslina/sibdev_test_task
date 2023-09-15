81ce432d22241220e4e51534bb5adcb36f8c90cf
FMaslina <FMaslina@yandex.ru>
2023-09-15 12:30:39 +0700

    Add 'rates' endpoint

--- currencies/quotes/urls.py
+++ currencies/quotes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from quotes.views import GetArchiveQuotes, AddCurrencyToTracked
+from quotes.views import GetArchiveQuotes, AddCurrencyToTracked, GetLastQuotes
 
 urlpatterns = [
     path('quotes/archive', GetArchiveQuotes.as_view(), name='get_archive_quotes'),
-    path('currency/user_currency', AddCurrencyToTracked.as_view(), name='add_currency_to_tracked')
+    path('currency/user_currency', AddCurrencyToTracked.as_view(), name='add_currency_to_tracked'),
+    path('rates', GetLastQuotes.as_view(), name='rates'),
 ]
