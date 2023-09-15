91cec742a591efb9b3379245299f88da7effc2f4
FMaslina <FMaslina@yandex.ru>
2023-09-15 11:32:30 +0700

    Add check_limits func to celery tasks

--- currencies/quotes/tasks.py
+++ currencies/quotes/tasks.py
@@ -1,7 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 from celery import shared_task
-from quotes.models import CurrencyModel, QuoteModel
+from django.core.mail import send_mail
+
+from quotes.models import CurrencyModel, QuoteModel, CurrencyUserModel
 
 
 @shared_task
@@ -14,5 +16,22 @@ def get_quotes():
             currency_obj = CurrencyModel.objects.create(currency_name=currency['CharCode'])
         else:
             currency_obj = currency_obj.first()
-        QuoteModel.objects.create(currency=currency_obj, quote=currency['Value'], date=datetime.now)
+        if QuoteModel.objects.filter(date=datetime.today()).exists():
+            pass
+        else:
+            QuoteModel.objects.create(currency=currency_obj, quote=currency['Value'], date=datetime.today())
     return data
+
+
+@shared_task
+def check_limits():
+    last_quotes_date = QuoteModel.objects.all().last().date
+    last_quotes = QuoteModel.objects.filter(date=last_quotes_date)
+    users_quotes_tracks = CurrencyUserModel.objects.all()
+
+    for user_quote in users_quotes_tracks:
+        daily_currency_quote = last_quotes.get(currency=user_quote.currency)
+        if daily_currency_quote.quote > user_quote.limit_value:
+            send_mail(f'Превышено пороговое значение по валюте {daily_currency_quote.currency.currency_name}',
+                      f'Превышено пороговое значение по {daily_currency_quote.currency.currency_name}, котировка - '
+                      f'{daily_currency_quote.quote}', 'FMaslina@yandex.ru', ['FMaslina@yandex.ru'])
