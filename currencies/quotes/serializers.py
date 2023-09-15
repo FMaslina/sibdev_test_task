050335e0149fe483d10210be5a7376eab22ff15d
FMaslina <FMaslina@yandex.ru>
2023-09-15 12:30:07 +0700

    Add QuoteModelSerializer

---
+++ currencies/quotes/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import QuoteModel
+
+
+class QuoteModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteModel
+        fields = '__all__'
